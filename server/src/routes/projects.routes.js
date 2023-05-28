ff1e59d4c5fb08fe5423b273caf12447434dedab
Yeraldinnesan <ysanabria92@hotmail.com>
2023-05-27 23:46:14 -0500

    documentation and server done

--- server/src/routes/projects.routes.js
+++ server/src/routes/projects.routes.js
@@ -7,10 +7,74 @@ import { getProjects } from "../controllers/projects/getProjects";
 
 const projectsRoutes = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Projects
+ *   description: Projects endpoints
+ */
+
+/**
+ * @swagger
+ * /projects:
+ *   get:
+ *     summary: Get all Projects
+ *     tags: [Projects]
+ */
 projectsRoutes.get("/", getProjects);
+
+/**
+ * @swagger
+ * /projects/{id}:
+ *   get:
+ *     summary: Get a Project
+ *     tags: [Projects]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ */
 projectsRoutes.get("/:id", getProject);
+
+/**
+ * @swagger
+ * /projects/{id}:
+ *   put:
+ *     summary: Update a Project
+ *     tags: [Projects]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ */
 projectsRoutes.put("/:id", updateProject);
+
+/**
+ * @swagger
+ * /projects/{id}:
+ *   delete:
+ *     summary: Delete a Project
+ *     tags: [Projects]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ */
 projectsRoutes.delete("/:id", deleteProject);
+
+/**
+ * @swagger
+ * /projects:
+ * post:
+ *   summary: Create a Project
+ *   tags: [Projects]
+ */
 projectsRoutes.post("/", createProject);
 
 export default projectsRoutes;
--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -8,11 +8,83 @@ import { updateTask } from "../controllers/tasks/updateTask.js";
 
 const tasksRoutes = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Tasks
+ *   description: Tasks endpoints
+ */
+
+/**
+ * @swagger
+ * /tasks:
+ *   get:
+ *     summary: Get all Tasks
+ *     tags: [Tasks]
+ */
 tasksRoutes.get("/", getTasks);
+
+/**
+ * @swagger
+ * /tasks/count:
+ *   get:
+ *     summary: Get count of all Tasks
+ *     tags: [Tasks]
+ */
 tasksRoutes.get("/count", getTaskCount);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   get:
+ *     summary: Get a Task
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ */
 tasksRoutes.get("/:id", getTask);
+
+/**
+ * @swagger
+ * /tasks:
+ *   post:
+ *     summary: Create a Task
+ *     tags: [Tasks]
+ */
 tasksRoutes.post("/", createTask);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   delete:
+ *     summary: Delete a Task
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ */
 tasksRoutes.delete("/:id", deleteTask);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   put:
+ *     summary: Update a Task
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ */
 tasksRoutes.put("/:id", updateTask);
 
 export default tasksRoutes;
